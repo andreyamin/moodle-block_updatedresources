1f202020d124e4d979d2fda9086b0f89305f81bb
Andre Yamin <andreyamin@gmail.com>
2014-02-19 16:48:53 -0300

    version update

--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php
-$plugin->version = 2014021000;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2014021801;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2013111800; // YYYYMMDDHH (This is the release version for Moodle 2.6)
